b41ce0cab2e1b9a6713660f8a2e0fcd62b45cc2e
HooMaN <HooMaN@DESKTOP-235ANIT>
2018-12-10 18:50:21 +0330

    یک مثال از BackgroundWorker An example of BackgroundWorker

--- BackGroundWorkerExample/Form1.Designer.cs
+++ BackGroundWorkerExample/Form1.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.lblPercent = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(186, 78);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(8, 49);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(339, 23);
+            this.progress.TabIndex = 1;
+            // 
+            // lblPercent
+            // 
+            this.lblPercent.AutoSize = true;
+            this.lblPercent.Location = new System.Drawing.Point(140, 15);
+            this.lblPercent.Name = "lblPercent";
+            this.lblPercent.Size = new System.Drawing.Size(85, 13);
+            this.lblPercent.TabIndex = 2;
+            this.lblPercent.Text = "Processing....0%";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(272, 78);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(355, 116);
+            this.Controls.Add(this.lblPercent);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Background Worker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.Label lblPercent;
+        private System.Windows.Forms.Button btnStop;
     }
 }
 
--- BackGroundWorkerExample/Form1.cs
+++ BackGroundWorkerExample/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,89 @@ namespace BackGroundWorkerExample
         public Form1()
         {
             InitializeComponent();
+        }
+        struct DataParameter
+        {
+            public int process;
+            public int delay;
+        }
+        private DataParameter _inputparameter;
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progress.Value = e.ProgressPercentage;
+            lblPercent.Text = string.Format("Processing...{0}%", e.ProgressPercentage);
+            progress.Update();
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int process = ((DataParameter)e.Argument).process;
+            int delay = ((DataParameter)e.Argument).delay;
+            int index = 1;
+            try
+            {
+                for (int i = 0; i < process; i++)
+                {
+                    if (!backgroundWorker.CancellationPending)
+                    {
+                        backgroundWorker.ReportProgress(index++ * 100 / process, string.Format("Process Data {0}", i));
+                        //used to simulate length of operation
+                        Thread.Sleep(delay);
+                        //add your code here......
+
+
+                        //We use the following code to run our code in a separate program from the original application
+                        //برای اینکه کد ما در ترید جدا از برنامه اصلی اجرا شود از کد زیر استفاده می کنیم
+
+                        // lblPercent.Invoke(new Action(() =>
+                        // {
+                        //    lblPercent.Text = i.ToString();
+                        // }));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                backgroundWorker.CancelAsync();
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show("Error.....", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (e.Cancelled)
+            {
+                MessageBox.Show("Process has been Canceled.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                progress.Value = 0;
+            }
+            else
+            {
+                MessageBox.Show("Process has been completed.", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                progress.Value = 0;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker.IsBusy)
+            {
+                _inputparameter.delay = 10;
+                _inputparameter.process = 1200;
+                backgroundWorker.RunWorkerAsync(_inputparameter);//دریافت نتیجه ترید توسط آرگومان
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker.IsBusy)
+            {
+                backgroundWorker.CancelAsync();
+            }
         }
     }
 }
